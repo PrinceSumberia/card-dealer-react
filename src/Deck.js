2077b3620d313cc73fd90fffc0610d2bd2a41570
Prince Sumberia <broughtmeup@gmail.com>
2020-01-07 13:59:42 +0530

    added axios to fetch data from api

--- src/Deck.js
+++ src/Deck.js
@@ -1,44 +1,52 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import './Deck.css';
+import axios from 'axios';
 
 class Deck extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { deckID: '', image: [], suit: [] };
+		this.state = { deckID: '', drawn: [] };
+		this.getCard = this.getCard.bind(this);
 	}
 
 	async componentDidMount() {
-		const data = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-		this.setState({ deckID: data.deck_id });
+		let response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+		this.setState({ deckID: response.data.deck_id });
+
+		//------------------ Using Fetch API ------------------------
+		// let response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+		// let data = await response.json();
+		// console.log(data);
 	}
 
-	async componentDidUpdate(prevState) {
-		const data = await fetch(`https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw`);
-		const image = data.image;
-		const suit = data.suit;
-		this.setState({ image: [ ...prevState.image, image ], suit: [ ...prevState.suit, suit ] });
+	async getCard() {
+		try {
+			const response = await axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/`);
+			const cards = response.data.cards;
+			this.setState((st) => ({
+				drawn: [
+					...st.drawn,
+					{
+						id: cards[0].code,
+						image: cards[0].image,
+						suit: cards[0].suit
+					}
+				]
+			}));
+		} catch (err) {
+			alert(err);
+		}
 	}
 
 	render() {
+		const cards = this.state.drawn.map((card) => <Card key={card.id} image={card.image} suit={card.suit} />);
 		return (
-			//  <div className='Deck'>
-			//   <h1 className='Deck-title'>♦ Card Dealer ♦</h1>
-			//   <h2 className='Deck-title subtitle'>
-			//     ♦ A little demo made with React ♦
-			//   </h2>
-			//   <button className='Deck-btn' onClick={this.getCard}>
-			//     Get Card!
-			//   </button>
-			//   <div className='Deck-cardarea'>{cards}</div>
-			// </div>
 			<div className="Deck">
 				<h1 className="Deck-title">♦ Card Dealer ♦</h1>
 				<h2 className="Deck-title subtitle">♦ A little demo made with React ♦</h2>
-				<button onClick={this.handleClick}>Give me a Card!</button>
-				<div className="Deck-cardarea">
-					<Card image={this.state.image} suit={this.state.suit} />
-				</div>
+				<button onClick={this.getCard}>Give me a Card!</button>
+				<div className="Deck-cardarea">{cards}</div>
 			</div>
 		);
 	}
